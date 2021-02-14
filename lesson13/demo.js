356623bb72ffdf66ee79c49e3c8e1d9f8d6303e9
Nadiia <hrihoryknadiia27@gmail.com>
2021-02-14 23:45:29 +0200

    ол

--- lesson13/demo.js
+++ lesson13/demo.js
@@ -1,7 +1,15 @@
 console.log("Операторы");
+let x = 3;
+let y = 5;
+console.log("x=", x, "y=", y, "x*y=", y*x);
+let remeinder = y % x;
+console.log(remeinder);
+x = x + 10;
+console.log(x);
+y += 10;
+console.log(y);
+y++;
+console.log(y);
 
-console.log("Условия");
-
-console.log("Циклы");
-
-console.log("Массивы");
+let w = x + 2 * y;
+console.log("w =", w)
