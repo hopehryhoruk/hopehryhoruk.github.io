92f278bbe1ade9c0ef167cbf4f1981e0cd142f3f
Nadiia <hrihoryknadiia27@gmail.com>
2021-03-16 21:00:50 +0200

    16.03

--- jsyoutube/app.js
+++ jsyoutube/app.js
@@ -1,30 +1,3 @@
-// 1 Зміні - сутності де ми зберігаємозначення і можемоміняти їх
-// camelCase = firstSecond
-//var name = "varieble"// старо, можна міняти  
-
-const last = "Minin"// не можна міняти!!!  //string
-let age = 26// можна міняти //number
-
-const isProgrammer = true //boolean
-
-//name = 'war;'
-console.log()
-
-
-// 2 Мутація
-console.log('Name ' + last + ' age ' + age)//age = text
-console.log(age)//age = namber
-
-//prompt(`Hello`)
-
-//3 Оператори
-let year = 2021
-const birst = 2005
-
-const myAge = year - birst
-
-console.log(myAge)
-
-console.log(++year)
-console.log(++year)
-console.log(year++)
+function greet(name){
+    
+}
